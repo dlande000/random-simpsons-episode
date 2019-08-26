6a5856e6f1e6d78f498c4a5db7995b8fb9afd80c
David Anderson <dlande000@gmail.com>
2019-08-26 16:25:07 -0400

    Add css file

--- simpsons.js
+++ simpsons.js
@@ -4,14 +4,15 @@ $(document).ready(() => {
     let episodeNum = 1;
     let headers;
 
-  $.ajax({
-   url:"simpsons_data.csv",
-   dataType:"text",
-   success:function(data)
-   {
-    const episodeData = data.split(/\r?\n|\r/);
-        episodeData.forEach((episode, i) => {
+    $.ajax({
+        url:"simpsons_data.csv",
+        dataType:"text",
+        success:function(data)
+        {
+            const episodeData = data.split(/\r?\n|\r/);
+            episodeData.forEach((episode, i) => {
             let episodeArr;
+
             if (i === 0) {
                 headers = episode.split(",");
             } else {
@@ -31,54 +32,56 @@ $(document).ready(() => {
                 episodeHash[episodeNum] = episodeObj;
                 episodeNum++;
             }
-        });
+            });
 
-        let inputSeasons = (position) => {
-            let seasonInput = ['<select name="seasons">'];
-            for (let seasonNum = 1; seasonNum < 31; seasonNum++) {
-                let toPush;
-                if ((position === "position1" && seasonNum === 1) || (position === "position2" && seasonNum === 10)) {
-                    toPush = "<option selected value=" + seasonNum + ">" + seasonNum + "</option>";
-                } else {
-                    toPush = "<option value=" + seasonNum + ">" + seasonNum + "</option>";
+            let inputSeasons = (position) => {
+                let seasonInput = ['<select name="seasons">'];
+                for (let seasonNum = 1; seasonNum < 31; seasonNum++) {
+                    let toPush;
+                    if ((position === "position1" && seasonNum === 1) || (position === "position2" && seasonNum === 10)) {
+                        toPush = "<option selected value=" + seasonNum + ">" + seasonNum + "</option>";
+                    } else {
+                        toPush = "<option value=" + seasonNum + ">" + seasonNum + "</option>";
+                    }
+                    seasonInput.push(toPush);
                 }
-                seasonInput.push(toPush)
+                seasonInput.push('</select>');
+                return seasonInput.join('');
+            };
+
+            $('#lower_seasons').html(inputSeasons("position1"));
+            $('#upper_seasons').html(inputSeasons("position2"));
+
+            $('#load_episode').click(function(){
+                let firstSeason = Number($('#lower_seasons option:selected').text());
+                let secondSeason = Number($('#upper_seasons option:selected').text());
+                let min;
+                let max;
+
+                if (firstSeason <= secondSeason) {
+                    min = firstSeason;
+                    max = secondSeason;
+                } else {
+                    min = secondSeason;
+                    max = firstSeason;
+                }
+
+                let randomInt = (Math.floor(Math.random() * (max - min + 1))) + min;
+                let randomSeason = seasonHash[randomInt];
+                let randomEpNum = randomSeason[Math.floor(Math.random() * randomSeason.length)];
+                let randomEp = episodeHash[randomEpNum];
+
+                let episodeHTML = `<div>
+                    <h2>${randomEp.Title}</h2>
+                    <h4>Season ${randomEp.Season} Episode ${randomEp.Episode}</h4>
+                    <img src=${randomEp.Image}></img>
+                    <p>${randomEp.Description}</p>
+                    <p>Original airdate: ${randomEp.Airdate}</p>
+                    <p>
+                    </div>`
+                
+                $('#episode_table').html(episodeHTML);
+                });
             }
-            seasonInput.push('</select>');
-            return seasonInput.join('');
-        };
-
-        $('#lower_seasons').html(inputSeasons("position1"));
-        $('#upper_seasons').html(inputSeasons("position2"));
-
-        $('#load_episode').click(function(){
-            let firstSeason = Number($('#lower_seasons option:selected').text());
-            let secondSeason = Number($('#upper_seasons option:selected').text());
-            let min;
-            let max;
-
-            if (firstSeason <= secondSeason) {
-                min = firstSeason;
-                max = secondSeason;
-            } else {
-                min = secondSeason;
-                max = firstSeason;
-            }
-
-            let randomInt = (Math.floor(Math.random() * (max - min + 1))) + min;
-            let randomSeason = seasonHash[randomInt];
-            let randomEpNum = randomSeason[Math.floor(Math.random() * randomSeason.length)];
-            let randomEp = episodeHash[randomEpNum];
-            let episodeHTML = `<div>
-                <h2>${randomEp.Title}</h2>
-                <h4>Season ${randomEp.Season} Episode ${randomEp.Episode}</h4>
-                <img src=${randomEp.Image}></img>
-                <p>${randomEp.Description}</p>
-                <p>Original airdate: ${randomEp.Airdate}</p>
-                <p>
-                </div>`
-            $('#episode_table').html(episodeHTML);
-        });
-   }
-  }); 
+    }); 
 });
